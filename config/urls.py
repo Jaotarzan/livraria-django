da5f5dcce7a6f6119ebaf75a1307b31694fbea5d
Jaotarzan <joaovsssouza59@gmail.com>
2024-05-28 17:24:00 -0300

    autenticação com simplejwt

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,12 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
 
 router = DefaultRouter()
@@ -14,4 +20,6 @@ router.register(r"livros", LivroViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
