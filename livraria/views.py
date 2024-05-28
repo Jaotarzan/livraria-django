5cb8543426cf230176ecef605787b0d03e527314
Jaotarzan <joaovsssouza59@gmail.com>
2024-05-28 11:21:41 -0300

    criaão dos serializers e view da model livro

--- livraria/views.py
+++ livraria/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Autor, Categoria, Editora
-from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer
+from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer, LivroListSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
@@ -15,3 +15,13 @@ class EditoraViewSet(ModelViewSet):
 class AutorViewSet(ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == "list":
+            return LivroListSerializer
+        elif self.action == "retrieve":
+            return LivroDetailSerializer
+        return LivroSerializer
