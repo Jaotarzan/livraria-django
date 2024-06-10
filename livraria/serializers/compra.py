e0ebe8d316c701940e918aee68ae3f0d4f3a181e
Jaotarzan <joaovsssouza59@gmail.com>
2024-06-10 10:51:52 -0300

    inclusão dos intens da compra na endpoint

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,8 +1,16 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"
+        depth = 1
 
 class CompraSerializer(ModelSerializer):
+    itens = ItensCompraSerializer(many=True, read_only=True)
     status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
     class Meta:
